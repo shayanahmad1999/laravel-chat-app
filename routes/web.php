402342fd25f230be4af837089131671954ca4584
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-12 15:36:02 +0500

    install ui of react with auth and set messages logics as well

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])
+    ->name('home');
+Route::get('/messages', [HomeController::class, 'messages'])
+    ->name('messages');
+Route::post('/message', [HomeController::class, 'message'])
+    ->name('message');
